4f095e13f6d37453aecea236b9d0a25eb296ffd1
anggabanny <anggabanny@gmail.com>
2019-08-01 19:08:00 +0700

    upload 1agust2019

---
+++ exercise-2.js
@@ -0,0 +1,10 @@
+function balikString(value){
+    var temp = '';
+    for(var i = value.length-1 ; i >=0 ; i--){
+        temp+=value[i];
+    }
+    return temp
+}
+console.log(balikString('hello world!'));
+console.log(balikString('hacktiv8'));
+console.log(balikString('anggabanny'));
