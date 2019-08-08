5e7fbe0aac1c70175d2a9f1338e75199ee4090d3
anggabanny <anggabanny@gmail.com>
2019-08-08 11:44:52 +0700

    💯

--- exercise-7.js
+++ exercise-7.js
@@ -3,18 +3,33 @@ function hitungJumlahKata(kalimat) {
         return 0
     }
 
-    var count = 1;
-    for(var i = 0; i < kalimat.length; i++){
-        if (kalimat[i] == ' ') {
-            count++;
+    var num = 0;
+    for(var k = kalimat.length-1; k > 0; k--){
+        if (kalimat[k] != ' ') {
+            break;
+        } else {
+            num++
+        }
+        console.log(kalimat[k]);
+    }
+    
+    var temp = [];
+    for(var i = 0; i < kalimat.length-num; i++){
+        if ((kalimat[i] != ' ' && kalimat[i+1] == ' ')){
+            temp.push(kalimat[i])
         }
     }
-    return count
+
+    if (kalimat[kalimat.length-1] == ' ') {
+        return temp.length
+    } else if (kalimat[kalimat.length-1] != ' ') {
+        return temp.length + 1
+    }
 }
 
 // TEST CASES
 console.log(hitungJumlahKata('I have a dream')); // 4
-console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6 
 console.log(hitungJumlahKata('A song to sing')); // 4
 console.log(hitungJumlahKata('I')); // 1
 console.log(hitungJumlahKata('I believe I can code')); // 5
